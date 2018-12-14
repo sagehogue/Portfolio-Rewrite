1435681835cc17922c4be3cc0752b61c9922504f
Sage Hogue <Sagehogue@gmail.com>
2018-12-13 21:20:20 -0800

    basic features of front page in place

--- ClientApp/src/components/TextModal/TextModal.js
+++ ClientApp/src/components/TextModal/TextModal.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './TextModal.module.css';
 
-const TextModal = (props) => {
-    return (
-        <div className='TextModal'>
-            {props.title? <h3>{props.title}</h3> : null}
-            {props.title? <hr /> : null}
-            {props.children}
-        </div>
-    )
+class TextModal extends Component {
+    state = {
+        isHovered: false
+    }
+
+    hoverOn = () => {
+        this.setState({ isHovered: true });
+    }
+
+    hoverOff = () => {
+        this.setState({ isHovered: false })
+    }
+
+    render() {
+        const classes = this.state.isHovered ? 'TextModal hover' : 'TextModal';
+        return (
+            <div className={classes}
+                onMouseEnter={this.hoverOn}
+                onMouseLeave={this.hoverOff}
+            >
+                {this.props.title ? <h3>{this.props.title}</h3> : null}
+                {this.props.title ? <hr /> : null}
+                {this.props.children}
+            </div>
+        )
+    }
 }
 
 export default TextModal;
--- ClientApp/src/containers/PageContainer/PageContainer.js
+++ ClientApp/src/containers/PageContainer/PageContainer.js
@@ -12,22 +12,30 @@ class PageContainer extends Component {
 
     splashPage = (
         <PageModal displayed={this.state.currentPage}>
-            <TextModal title="Hi. I'm Sage.">
-                I'm a PDX-based web developer and tech enthusiast.
-                </TextModal>
+            <TextModal title="Hi. I'm Sage." >
+                <p>I'm a PDX-based web developer and tech enthusiast.</p>
+            </TextModal>
             {/* <TextModal>
                     <span className="titleWord wordWeb">web</span>
                     <span className="titleWord wordSage">Sage</span> is live!
                 </TextModal> */}
-            <TextModal >
-                Welcome to my website!
-                </TextModal>
-            <TextModal>Great job Sage, but keep up the good work!</TextModal>
+            <TextModal title='Welcome to my website.'>
+                {/* <p>To learn more, click a button below.</p> */}
+            </TextModal>
+            <TextModal title='My work'>
+                <p>vistelse app</p>
+            </TextModal>
+            <TextModal title="Who am I?">
+                <p>What's my name? Learn a little something about me.</p>
+            </TextModal>
+            <TextModal>
+                <p>Great job Sage, but keep up the good work!</p>
+            </TextModal>
         </PageModal>
     )
 
     render() {
-        return this.state.currentPage === "splash"? this.splashPage : null;
+        return this.state.currentPage === "splash" ? this.splashPage : null;
     }
 }
 
