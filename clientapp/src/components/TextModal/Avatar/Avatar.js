49eeb938bc84863d434f13a490048aa139d2f6df
Sage Hogue <Sagehogue@gmail.com>
2018-12-17 23:37:00 -0800

    wows! typing bubble anims done, dug my way out of some holes.

--- clientapp/src/components/TextModal/Avatar/Avatar.js
+++ clientapp/src/components/TextModal/Avatar/Avatar.js
@@ -3,6 +3,8 @@ import React from 'react';
 import TextBubble from './TextBubble/TextBubble';
 import classes from './Avatar.module.css';
 
+// add 'animating' state that cycles between the three items 
+
 const Avatar = (props) => {
     return (
         <div className={classes.avatar}>
--- clientapp/src/components/TextModal/Avatar/TextBubble/TextBubble.js
+++ clientapp/src/components/TextModal/Avatar/TextBubble/TextBubble.js
@@ -3,25 +3,94 @@ import React, { Component } from 'react';
 import classes from "./TextBubble.module.css";
 
 class textBubble extends Component {
-    
-    componentDidMount() {
 
+    state = {
+        animating: false, // 'first', 'second', 'third', refers to which div 
+
+    }
+
+    componentDidMount() {
+        const newIntervalID = setInterval(this.animateDots, 400)
+        this.intervalID = newIntervalID;
+    }
+
+    componentDidUpdate() {
     }
 
     componentWillUnmount() {
-
+        clearInterval(this.intervalID);
     }
-    
+
+    getDotToAnimate() {
+        switch (this.state.animating) {
+            case 'first':
+                return 'second';
+            case 'second':
+                return 'third';
+            case 'third':
+                return 'delayed';
+            case 'delayed':
+                return false;
+            default:
+                return 'first';
+        }
+    }
+
+    animateDots = () => {
+        const dotToAnimate = this.getDotToAnimate();
+        console.log(dotToAnimate);
+        this.setState({ animating: dotToAnimate });
+    }
+
     render() {
-        
-        return (
-            <div className={classes.bubble}>
-                <div className={classes.dot}></div>
-                <div className={classes.dot}></div>
-                <div className={classes.dot}></div>
-            </div>
-        )
+        switch (this.state.animating) {
+
+            case 'first':
+                return (
+                    <div className={classes.bubble}>
+                        <div className={`${classes.dot} ${classes.animating}`}></div>
+                        <div className={classes.dot}></div>
+                        <div className={classes.dot}></div>
+                    </div>
+                )
+
+            case 'second':
+                return (
+                    <div className={classes.bubble}>
+                        <div className={classes.dot}></div>
+                        <div className={`${classes.dot} ${classes.animating}`}></div>
+                        <div className={classes.dot}></div>
+                    </div>)
+
+            case 'third':
+                return (<div className={classes.bubble}>
+                    <div className={classes.dot}></div>
+                    <div className={classes.dot}></div>
+                    <div className={`${classes.dot} ${classes.animating}`}></div>
+                </div>)
+            case 'delayed':
+                return (<div className={classes.bubble}>
+                    <div className={classes.dot}></div>
+                    <div className={classes.dot}></div>
+                    <div className={classes.dot}></div>
+                </div>)
+            default:
+                return (<div className={classes.bubble}>
+                    <div className={classes.dot}></div>
+                    <div className={classes.dot}></div>
+                    <div className={classes.dot}></div>
+                </div>)
+        }
     }
 }
+// const bubble = (
+//     <div className={classes.bubble}>
+//         {(this.state.animating === 'first' ? <div className={`${classes.dot} ${classes.animating}`}></div> : <div className={classes.dot}></div>)}
+{/* {this.state.animating === 'second' ? <div className={`${classes.dot} ${classes.animating}`}></div>
+                                : <div className={classes.dot}></div>} */}
+{/* {this.state.animating === 'third' ? <div className={`${classes.dot} ${classes.animating}`}></div>
+                                : <div className={classes.dot}></div>} */}
+//     </div>
+// )
 
 export default textBubble
