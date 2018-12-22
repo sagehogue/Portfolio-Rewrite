c71ef7e300df366955aeb7703079c663af01d0c9
Sage Hogue <Sagehogue@gmail.com>
2018-12-22 15:43:13 -0800

    began scaffolding of story app api

--- clientapp/src/pages/StoryPage/StoryPage.js
+++ clientapp/src/pages/StoryPage/StoryPage.js
@@ -3,34 +3,136 @@ import React, { Component } from 'react';
 import PageModal from '../../components/PageModal/PageModal';
 import TextModal from '../../components/TextModal/TextModal';
 
+// I like the idea of messages coming into view from left, exiting to the right. Nice and orderly, like a play!
+
 import classes from './StoryPage.module.css';
+
+
+// Def need to code in some authentication!!!
 
 class storyPage extends Component {
     state = {
         // modalInView: false, not rdy
         display: {
-            introMessage: true,
+            introMessage: false,
         },
+        storyCollection: [],
+        titleCollection: [],
 
     }
 
-    ComponentDidMount() {
-        this.storyIntro()
+    componentDidMount() {
+        if (!localStorage.getItem('storyInfoLoaded')) {
+            this.getAllStories()
+        } else {
+            const retrievedStories = JSON.parse(localStorage.getItem('stories'))
+            console.log(retrievedStories);
+            console.log('used local storage!')
+            // this.setState({
+                // Logic to get localstorage, JSON.parse it, iterate through resulting object and set state with the story objects.
+            // })
+        }
+        console.log('mounted!')
+        // this.getAllStories();
+    }
+
+    componentDidUpdate() {
+
+    }
+
+    async getAllStories() {
+        fetch(this.apiPaths.everything).then(res => res.json())
+            .then((result) => {
+                localStorage.setItem('stories', JSON.stringify(result));
+                result.forEach((story) => {
+                    this.setState((oldState) => {
+                        let newArray = [...this.state.storyCollection];
+                        newArray.push(story)
+                        const newState = {
+                            ...oldState,
+                            storyCollection: newArray
+                        }
+                        return newState;
+                    })
+                    // this.state.titleCollection.push(story.title)
+                    console.log(this.state.storyCollection);
+                })
+                this.setState({
+                    storyInfoLoaded: true,
+                })
+                localStorage.setItem('storyInfoLoaded', 'true')
+            })
+    }
+
+    async getTitleArray() {
+        fetch(this.apiPaths.everything).then(res => res.json())
+            .then((result) => {
+                result.forEach((story) => {
+                    this.setState((oldState) => {
+                        let newArray = [...this.state.titleCollection];
+                        newArray.push(story.title)
+                        const newState = {
+                            ...oldState,
+                            titleCollection: newArray
+                        }
+                        return newState;
+                    })
+                    // this.state.titleCollection.push(story.title)
+                    console.log(this.state.titleCollection);
+                })
+                this.setState({
+                    storyTitlesLoaded: true,
+                })
+                this.storyIntro()
+            }
+            )
+    }
+
+    apiPaths = {
+        // gets all test data
+        everything: "https://localhost:5001/api/story",
+        // TODOS:
+        // storyTitles: ".../api/story/titles"
+        // requestStory: ".../api/story/{requestedstory}"
     }
 
     storyIntro() {
         this.setState({ display: { intro: true } })
     }
 
+    testButton = (
+        <div className={classes.testButton} onClick={() => {
+            localStorage.clear()
+        }}>
+        localStorage.clear()
+        </div>
+    )
+
+    introMessages = {
+        first: (
+            <div className={`${classes.introModal} ${classes.textModal} ${classes.firstIntro} ${this.state.display.introMessage ? '' : classes.Hide}`}>
+                <TextModal>
+                    <p>Welcome to the story page.</p>
+                </TextModal>
+            </div>
+        ),
+        second: (
+            <div className={`${classes.introModal} ${classes.textModal} ${this.state.display.introMessage ? '' : classes.Hide} ${classes.secondIntro}`}>
+                <TextModal>
+                    <p>This is a 'choose your own adventure' experience.</p>
+                </TextModal>
+            </div>
+        ),
+    }
 
     render() {
         return (
             <PageModal className={"story"} onScreen={this.state.modalInView}>
                 <div className={`${classes.pageWrapper}`}>
-                    <div className={`${classes.introModal} ${classes.textModal} ${this.state.display.introMessage ? '' : classes.Hide}`}>
-                        <TextModal>
-                            Welcome to the story page.
-                        </TextModal>
+                    {this.introMessages.first}
+                    {this.introMessages.second}
+                    <div className={classes.ButtonBox}>
+                    {this.testButton}
                     </div>
                 </div>
             </PageModal>
