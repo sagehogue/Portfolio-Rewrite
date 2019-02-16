13a1cba501ecb01a941b55a499367057a42f5ed2
Sage Hogue <Sagehogue@gmail.com>
2019-02-16 08:56:21 -0800

    added about me item

--- ClientApp/src/pages/PortfolioPage/PortfolioPage.js
+++ ClientApp/src/pages/PortfolioPage/PortfolioPage.js
@@ -38,11 +38,16 @@ class PortfolioPage extends Component {
         return (
             <PageModal displayed={"portfolio"}>
                 <div className={wrapperClassList.join(' ')}>
-                    <PortfolioItem label="This Website" position={1} stateHandler={this.displayItem}>
+                    <PortfolioItem label="About Me" position={1} stateHandler={this.displayItem}>
+                        <section>
+                            <p>Here is information about me</p>
+                        </section>
+                    </PortfolioItem>
+                    <PortfolioItem label="This Website" position={2} stateHandler={this.displayItem}>
                         <section>
                             <h3>Construction Details</h3>
-                            <p>I originally started this website to provide direction for my learning during code school and to serve as a visual repository for my projects. I initially built it with a vanilla JavaScript front-end and a python backend powered by Django. I was very much learning everything as I went, and so the resulting product was ugly and of poor quality. 
-                                
+                            <p>I originally started this website to provide direction for my learning during code school and to serve as a visual repository for my projects. I initially built it with a vanilla JavaScript front-end and a python backend powered by Django. I was very much learning everything as I went, and so the resulting product was ugly and of poor quality.
+
                             </p>
                             <p>After code school, I decided that refactoring this website would be the perfect opportunity for me to learn a framework I had an eye on, React, and so I basically remade this from scratch using React for my front-end and Google's Firebase for my data hosting needs. </p>
                             <p>ES6</p>
@@ -61,7 +66,7 @@ class PortfolioPage extends Component {
                             <p></p>
                         </section>
                     </PortfolioItem>
-                    <PortfolioItem label="Coursework" position={2} stateHandler={this.displayItem}>
+                    <PortfolioItem label="Projects" position={3} stateHandler={this.displayItem}>
                         <section>
                             <h3>i did all this stuff at code school</h3>
                             <p>angry dice</p>
@@ -70,12 +75,12 @@ class PortfolioPage extends Component {
                             <p>django stuff</p>
                         </section>
                     </PortfolioItem>
-                    <PortfolioItem label="Volunteer Work" position={3} stateHandler={this.displayItem}>
+                    <PortfolioItem label="Volunteer Work" position={4} stateHandler={this.displayItem}>
                         <section>
                             <p>Women's Crisis Center IT work</p>
                         </section>
                     </PortfolioItem>
-                    <PortfolioItem label="Work Experience" position={4} stateHandler={this.displayItem}>
+                    <PortfolioItem label="Work Experience" position={5} stateHandler={this.displayItem}>
                         <section>
                             <p>Vagabond Brewery - <i>Prep Cook</i></p>
                         </section>
