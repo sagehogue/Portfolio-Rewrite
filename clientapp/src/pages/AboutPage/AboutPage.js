e4f72a03e64ffa8a0632d4b2fa2c3d45c5aac112
Sage Hogue <Sagehogue@gmail.com>
2019-03-02 18:56:19 -0800

    further progress on aboutpage

--- clientapp/src/pages/AboutPage/AboutPage.js
+++ clientapp/src/pages/AboutPage/AboutPage.js
@@ -42,7 +42,10 @@ class aboutPage extends Component {
                         experiment scrollable>
                         <section>
                             <h3>Cali born, Portland raised</h3>
-                            <p>On a Friday night in Chico in the late 90's, a star was born. ;)</p>
+                            <p>I was born on a Friday night in the 90’s in a college town known as Chico. I moved to Portland as a young child and was raised there, immersed in the unique community and humanitarian culture of the town throughout my teens and early adulthood. I entered the workforce through the food service/catering industry, getting my start at a bustling local deli and moving on to do corporate catering work for locations such as Google and the University of Portland.</p>  <p>I soon grew to tire of the repetitive nature of the jobs I was able to get, and so I began to seek a more meaningful and gratifying way to earn a living. However, being a young man with little guidance or plan to speak of, I had no idea how to approach that. How could I develop expertise largely independently, open a career path, and build a future for myself?</p>
+                            <p>After reviewing my options, I decided to give web development a shot. I have long held a general appreciation for programming, but until I took my bootcamp at PDX Code Guild I was largely ignorant to what it actually was, and had never tackled anything more complex than a HTML web page. It was quite the leap of faith for me, having no real idea what I was getting myself into or even if I would be interested in it after a few months had passed, but my gambit proved a successful one, and after a few short months not only did I know how to create a full-stack website, but I had found a professional passion and industry to boot.</p> <p>Since bootcamp graduation, I have spent my time expanding my skillset and learning more about the industry I have chosen. There’s an unlimited amount of information and ability out there to be freely gained, and I ambitiously seek it out to arm my future self with the best possible problem solving techniques I can.</p>
+
+
                         </section>
                     </PortfolioItem>
                     <PortfolioItem label="Mission"
@@ -82,6 +85,7 @@ class aboutPage extends Component {
                     </PortfolioItem>
                 </div>
                 <div className={classes.Banner}>
+
                     <div className={classes.bannerStripe} />
                     <span className={[classes.firstTypography, classes.bannerTypography].join(' ')}>
                         Thanks for taking an interest!
@@ -90,6 +94,7 @@ class aboutPage extends Component {
                     <span className={[classes.secondTypography, classes.bannerTypography].join(' ')}>
                         Click any of the categories and scroll to see the full text.
                         </span>
+                    <div className={classes.bannerStripe} />
                     <div className={classes.bannerStripe} />
                 </div>
             </PageModal>
